7b45d66aa886918e4e012222b7e2a0711857e67b
Austin Tsai <aytsai@usc.edu>
2025-05-01 14:17:32 -0700

    database script

---
+++ final_project.sql
@@ -0,0 +1,43 @@
+drop database if exists final;
+create database final;
+use final;
+
+create table Users(
+	SID int auto_increment primary key,
+    username varchar(100) not null,
+    pass varchar(100) not null,
+    rating float not null,
+    email varchar(100) not null,
+    token varchar(400) not null
+);
+
+create table Product(
+    ID int auto_increment primary key,
+    product_name varchar(100) not null,
+    price float not null,
+    descript varchar(250) not null,
+    thumbnail varchar(500) not null,
+    tag varchar(100) not null,
+    sellerID int not null,
+    foreign key (sellerID) references Users(SID)
+);
+
+create table Conversation(
+    id int auto_increment primary key,
+    user1ID int not null,
+    user2ID int not null,
+    unread bool not null,
+    listing_name varchar(50) not null,
+    foreign key (user1ID) references Users(SID),
+    foreign key (user2ID) references Users(SID)
+);
+
+create table Messages (
+    id int auto_increment primary key,
+    Text text not null,
+    SenderID int not null,
+    ReceiverID int not null,
+    timeStamp datetime not null,
+    foreign key (SenderID) references Users(SID),
+    foreign key (ReceiverID) references Users(SID)
+);
