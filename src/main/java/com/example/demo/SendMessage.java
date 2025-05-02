da9ebeddb4ce558bb8dc6d15fb844053ab628223
Austin Tsai <aytsai@usc.edu>
2025-05-02 15:03:52 -0700

    sending messages WIP

---
+++ src/main/java/com/example/demo/SendMessage.java
@@ -0,0 +1,102 @@
+package com.uscmarketplace;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Cookie;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@WebServlet("/send-message-servlet")
+public class SendMessage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Value("${spring.datasource.url}")
+	private String db;
+	
+	@Value("${spring.datasource.username}")
+	private String dbUsername;
+	
+	@Value("${spring.datasource.password}")
+	private String dbPassword;
+       
+    public SendMessage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// Retrieving variables to verify log in
+		String sender = request.getParameter("yourUserID");
+		String reciever = request.getParameter("otherUserID");
+		PrintWriter out = response.getWriter();
+		
+		Connection conn = null;
+		Statement st = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		try {
+			Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+			String senderID = request.getParameter("yourUserID");
+			String recieverID = request.getParameter("otherUserID");
+			String message = request.getParameter("message");
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(db, dbUsername, dbPassword);
+			String sql = "INSERT INTO Messages (message, senderID, receiverID, timestamp) VALUES (?, ?, ?, ?)";			
+			ps = conn.prepareStatement(sql);
+			
+			ps.setString(1, message);
+			ps.setString(2, sender);
+			ps.setString(3, reciever);
+			ps.setTimestamp(4, timestamp);
+			ps.executeUpdate();
+			
+		} catch (SQLException sqle) {
+			System.out.println ("SQLException: " + sqle.getMessage());
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (st != null) {
+					st.close();
+				}
+				if (ps != null) {
+					ps.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException sqle) {
+				System.out.println("sqle: " + sqle.getMessage());
+			}
+		}
+	}
+}
