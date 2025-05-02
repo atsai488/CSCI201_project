503222411f65bf2081e1e44c75a4c4b359650508
Austin Tsai <aytsai@usc.edu>
2025-05-02 10:37:32 -0700

    Revert "Merge branch 'feature/login-reg'"
    This reverts commit 1f497145b285ab66b6ad64ed65850fe97cd852d8, reversing
    changes made to 366be53be7c6f84a51d824fced709f2d7f1b07a0.

--- src/main/java/com/example/demo/UscMarketplaceApplication.java
+++ src/main/java/com/example/demo/UscMarketplaceApplication.java
@@ -1,17 +1,13 @@
-package com.uscmarketplace;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@ServletComponentScan
 public class UscMarketplaceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UscMarketplaceApplication.class, args);
 	}
-	
+
 }
