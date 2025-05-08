3314c742a25e7e3c1b156b0b53d16135153e5e93
Austin Tsai <aytsai@usc.edu>
2025-05-08 16:06:22 -0700

    sql database fix

--- final_project.sql
+++ final_project.sql
@@ -4,10 +4,10 @@ use final;
 
 CREATE TABLE Users (
 	SID INT AUTO_INCREMENT PRIMARY KEY,
-	fname VARCHAR(255) NOT NULL,
-    lname VARCHAR(255) NOT NULL,
+	username VARCHAR(255) NOT NULL,
 	email VARCHAR(255) UNIQUE NOT NULL,
 	password VARCHAR(255) NOT NULL,
+    rating float not null,
     role ENUM('buyer', 'seller') NOT NULL 
 );
 
